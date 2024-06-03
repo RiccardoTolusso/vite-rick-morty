1b497cbbcb183e4947137adb430df9cf4c5c00d7
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-06-03 14:14:48 +0200

    UPD: created template structure with comments

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <!-- <script></script> -->
 <template>
+<!-- HEADER -->
+  <!-- title -->
+  <!-- search bar -->
+<!-- /HEADER -->
 
+<!-- MAIN -->
+  <!-- card -->
+    <!-- img -->
+    <!-- title name -->
+    <!-- vivo o morto -->
+    <!-- razza -->
+  <!-- /card -->
+<!-- /MAIN -->
+
+<!-- FOOTER -->
+  <!-- numero di personaggi trovati -->
+<!-- /FOOTER -->
 </template>
 <!-- <style></style> -->
